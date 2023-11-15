f430e4e35637dd3f3b165b77fb7b7b3b54387421
Jacob M Lovegren <jmlovegr@csciarch03.muad.local>
2023-11-15 16:28:51 -0500

    made some changes

--- main.cc
+++ main.cc
@@ -14,8 +14,8 @@
 /******************************************************************************/
 
 
-std::filesystem::path 
-getFileName ();
+//std::filesystem::path 
+//getFileName ();
 
 /******************************************************************************/
 
@@ -23,16 +23,17 @@ int main (int argc, char ** argv)
 {
     InitializeMagick(*argv);
 
-    std::filesystem::path gifInfile = getFileName();
+    //std::filesystem::path gifInfile = getFileName();
 
 
     // Construct the image object. Separating image construction from the 
     // the read operation ensures that a failure to read the image file 
     // doesn't render the image object useless. 
     Image image;
-    try { 
+    try 
+    { 
         // Read a file into image object 
-        image.read(infile.c_str());
+        image.read("Animhorse.gif");
 
         // Crop the image to specified size (width, height, xOffset, yOffset)
         image.crop( Geometry(100,100, 100, 100) );
@@ -41,15 +42,16 @@ int main (int argc, char ** argv)
         image.write( "x.gif" ); 
     } 
     catch( Exception &error_ ) 
-        { 
+    { 
         std::cout << "Caught exception: " << error_.what() << std::endl; 
         return 1; 
-        } 
+    } 
     return 0; 
 }
 
 /******************************************************************************/
 
+/*
 std::filesystem::path 
 getFileName ()
 {
@@ -58,4 +60,4 @@ getFileName ()
   std::cin >> infile;
   std::cout << "\n";
   return infile;
-}
+}*/
