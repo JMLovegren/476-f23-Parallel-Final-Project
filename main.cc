ed35238e3e7c2a23a321da63f314b9325491953e
JMLovegren <jmlovegr@millersvile.edu>
2023-11-30 11:53:47 -0500

    stuff

--- main.cc
+++ main.cc
@@ -1,3 +1,12 @@
+/*
+  Filename   : main.cc
+  Author     : Jacob Lovegren, Timothy Moser
+  Course     : CSCI 476
+  Date       : 11/30/2023
+  Assignment : Final Project - Deepfried Memes
+  Description: Make deep fried memes out of gifs in
+*/
+
 #include <filesystem>
 #include <iostream>
 #include <string>
@@ -8,6 +17,8 @@
 #include <Magick++.h> 
 #include "Timer.hpp"
 #include "BS_thread_pool.hpp"
+
+/******************************************************************************/
 
 using namespace Magick; 
 
@@ -26,12 +37,17 @@ void
 serialProcess (std::vector<Image>& seriImages, int oilNum);
 
 void
+parallelSpatialProcess (std::vector<Image>& paraImages, int oilNum, 
+                        int numThreads);
+
+void
 parallelProcess (std::vector<Image>& paraImages, int oilNum, int numThreads);
 
 void
 openMP (std::vector<Image>& ompImages, int oilNum);
 
-void convertToGIF(std::vector<Image>& images, const std::string& outputGifPath);
+void
+convertToGIF(std::vector<Image>& images, const std::string& outputGifPath);
 
 /******************************************************************************/
 
@@ -46,6 +62,7 @@ int main (int argc, char **argv)
   size_t numFrames = findNumFrames (infileString);
 
   std::vector<Image> seriImages = makeImageObjects (infileString, numFrames);
+  std::vector<Image> paraSpacImages = seriImages;
   std::vector<Image> paraImages = seriImages;
   std::vector<Image> ompImages = seriImages;
   Timer timer;
@@ -56,39 +73,52 @@ int main (int argc, char **argv)
   timer.stop ();
   double serProcTime = timer.getElapsedMs ();
   timer.start ();
-  std::string outputGifPath = infile.stem().string() +  std::to_string (oilNum) + "SeriMod.gif";
+  std::string outputGifPath = infile.stem().string() +  
+                                std::to_string (oilNum) + "SeriMod.gif";
   convertToGIF(seriImages, outputGifPath);
   timer.stop();
   double serZipTime = timer.getElapsedMs ();
 
-  // Parallel
+  // Parallel with locality
+  timer.start ();
+  parallelSpatialProcess (paraSpacImages, oilNum, numThreads);
+  timer.stop ();
+  double paraSpacProcTime = timer.getElapsedMs ();
+  outputGifPath = infile.stem().string() + 
+                    std::to_string (oilNum) + "ParaSpacMod.gif";
+  convertToGIF (paraSpacImages, outputGifPath);
+
+  // Parallel without locality
   timer.start ();
   parallelProcess (paraImages, oilNum, numThreads);
   timer.stop ();
   double paraProcTime = timer.getElapsedMs ();
-  timer.start ();
-  outputGifPath = infile.stem().string() +  std::to_string (oilNum) + "ParaMod.gif";
+  outputGifPath = infile.stem().string() + 
+                    std::to_string (oilNum) + "ParaMod.gif";
   convertToGIF (paraImages, outputGifPath);
-  timer.stop ();
-  double paraZipTime = timer.getElapsedMs ();
 
   // OpenMP
   timer.start();
   openMP(ompImages, oilNum);
   timer.stop();
   double ompProcTime = timer.getElapsedMs();
-  timer.start();
-  outputGifPath = infile.stem().string() + std::to_string (oilNum) + "OmpMod.gif";
+  outputGifPath = infile.stem().string() + 
+                    std::to_string (oilNum) + "OmpMod.gif";
   convertToGIF(ompImages, outputGifPath);
-  timer.stop();
-  double ompZipTime = timer.getElapsedMs();
 
-  fmt::print ("Serial time: {:.2f} ms\nZip Time: {:.2f} ms\n", serProcTime, serZipTime);
-  fmt::print ("Parallel time: {:.2f} ms\nZip Time: {:.2f} ms\n", paraProcTime, paraZipTime);
-  fmt::print ("Speedup: {:.2f}\n", serProcTime / paraProcTime);
+  // output
+  fmt::print ("Frames: {}\n\n", numFrames);
 
-  fmt::print("OpenMP time: {:.2f} ms\nZip Time: {:.2f} ms\n", ompProcTime, ompZipTime);
-  fmt::print("Open MP Speedup: {:.2f}\n", serProcTime / paraProcTime);
+  fmt::print ("Zip Time:                   {:.2f}\n", serZipTime);
+
+  fmt::print ("Serial time:                {:.2f} ms\n", serProcTime);
+  fmt::print ("Parallel time w/ Locality:  {:.2f} ms\n", paraSpacProcTime);
+  fmt::print ("Parallel time w/o Locality: {:.2f} ms\n", paraProcTime);
+  fmt::print ("OpenMP time:                {:.2f} ms\n", ompProcTime);
+
+  fmt::print ("Parallel Local Speedup:     {:.2f}\n", serProcTime / paraSpacProcTime);
+  fmt::print ("Parallel no Local Speedup:  {:.2f}\n", serProcTime / paraProcTime);
+  fmt::print ("Open MP Speedup:            {:.2f}\n", serProcTime / ompProcTime);
 
   return 0; 
 } 
@@ -127,10 +157,7 @@ findNumFrames (std::string& infileString)
   return temp.scene() + 1;
 }
 
-
-
-
-
+/******************************************************************************/
 
 std::vector<Image> 
 makeImageObjects (std::string& infileString, size_t numFrames)
@@ -145,23 +172,30 @@ makeImageObjects (std::string& infileString, size_t numFrames)
   return images;
 }
 
-
+/******************************************************************************/
 
 void
 serialProcess (std::vector<Image>& seriImages, int oilNum)
 {
   for (size_t i = 0; i < seriImages.size (); ++i)
   {
-    seriImages[i].oilPaint(oilNum);
+    seriImages[i].oilPaint ();
+    seriImages[i].blur (5.0, 0.5);
+    seriImages[i].modulate(100, 500, 100);
+    seriImages[i].contrast (true);
+    seriImages[i].contrast (true);
+    seriImages[i].addNoise(Magick::UniformNoise);
+    seriImages[i].swirl(60);
   }
 }
 
+/******************************************************************************/
 
 void
-parallelProcess (std::vector<Image>& paraImages, int oilNum, int numThreads)
+parallelSpatialProcess (std::vector<Image>& paraImages, int oilNum, int numThreads)
 {
   // Constructs a thread pool with only p threads.
-  BS::thread_pool pool(numThreads);
+  BS::thread_pool pool (numThreads);
 
   for (int threadCtr = 0; threadCtr < numThreads; ++threadCtr)
   {
@@ -171,16 +205,68 @@ parallelProcess (std::vector<Image>& paraImages, int oilNum, int numThreads)
     pool.push_task ([=, &paraImages] 
       {
         for (auto i = lowRange; i < highRange; ++i)
-          {
+        {
           paraImages[i].oilPaint ();
-          }
+          paraImages[i].blur (5.0, 0.5);
+          paraImages[i].modulate(100, 500, 100);
+          paraImages[i].contrast (true);
+          paraImages[i].contrast (true);
+          paraImages[i].addNoise(Magick::UniformNoise);
+          paraImages[i].swirl(60);
+        }
       }
     );
   }
   pool.wait_for_tasks ();
 }
 
+/******************************************************************************/
 
+void
+parallelProcess (std::vector<Image>& paraImages, int oilNum, int numThreads)
+{
+  // Constructs a thread pool with only p threads.
+  BS::thread_pool pool (numThreads);
+
+  for (size_t i = 0; i < paraImages.size (); ++i)
+  {
+    pool.push_task ([=, &paraImages] 
+      {
+
+        paraImages[i].oilPaint ();
+        paraImages[i].blur (5.0, 0.5);
+        paraImages[i].modulate(100, 500, 100);
+        paraImages[i].contrast (true);
+        paraImages[i].contrast (true);
+        paraImages[i].addNoise(Magick::UniformNoise);
+        paraImages[i].swirl(60);
+
+      }
+    );
+  }
+  pool.wait_for_tasks ();
+}
+
+/******************************************************************************/
+
+void
+openMP (std::vector<Image>& ompImages, int oilNum)
+{
+#pragma omp parallel for
+  for (size_t i = 0; i < ompImages.size (); ++i)
+  {
+    ompImages[i].oilPaint ();
+    ompImages[i].blur (5.0, 0.5);
+    ompImages[i].modulate(100, 500, 100);
+    ompImages[i].contrast (true);
+    ompImages[i].contrast (true);
+    ompImages[i].addNoise(Magick::UniformNoise);
+    ompImages[i].swirl(60);
+
+  }
+}
+
+/******************************************************************************/
 
 void convertToGIF(std::vector<Image> & images, const std::string& outputGifPath) 
 {
@@ -202,12 +288,4 @@ void convertToGIF(std::vector<Image> & images, const std::string& outputGifPath)
   }
 }
 
-void
-openMP (std::vector<Image>& ompImages, int oilNum)
-{
-  #pragma omp parallel for
-  for (size_t i = 0; i < ompImages.size (); ++i)
-  {
-    ompImages[i].oilPaint(oilNum);
-  }
-}
+/******************************************************************************/
